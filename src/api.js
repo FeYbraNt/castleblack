4c0bb0472775898fb8e5b5d436b300a7b3f88269
FeYbraNt <rldorado86@gmail.com>
2019-11-11 13:54:41 +0100

    Building first requests

--- src/api.js
+++ src/api.js
@@ -1,33 +1,54 @@
 const { Router } = require("express");
 const api = Router();
 
-// This will be your data source
-const players = [
-  { id: 1, name: "Jon Snow", age: 23, health: 100, bag: [1] },
-  { id: 2, name: "Littlefinger", age: 35, health: 100, bag: [2] },
-  { id: 3, name: "Daenerys Targaryen", age: 20, health: 100, bag: [3] },
-  { id: 4, name: "Samwell Tarly", age: 18, health: 100, bag: [4] }
-];
-const objects = [
-  { id: 1, name: "spoon", value: -1 },
-  { id: 2, name: "knife", value: -10 },
-  { id: 3, name: "sword", value: -20 },
-  { id: 4, name: "potion", value: +20 }
-];
+// Data moved to dummy database file
+const db = require("./data/db")
 
 // EXAMPLE ENDPOINT: LIST ALL OBJECTS
-api.get("/objects", function(req, res) {
-  res.json(objects);
+api.get("/objects", (req, res) => {
+  res.json(db.objects);
 });
 
-/* Create all routes for API */
-api.get("/", function(req, res) {
-  res.status(200).send({ message: 'Welcome to Castle Black API' })
+// 0. Welcome message
+api.get("/", (req, res) => {
+  res.status(200).send('Welcome to Castle Black API')
 })
 
 // 1. List all players
-api.get("/players", function(req, res) {
-  res.json(players)
+api.get("/players", (req, res) => {
+  res.json(db.players)
 })
+
+// 2. Create player: adds a new player to data source.
+api.post('/player', (req, res) => {
+  if (!req.body.name) {
+    res.status(502).send('Name is required')
+  } else {
+    const player = { id: db.players.length + 1, name: req.body.name }
+    res.status(200).send('Player ' + player.name + ' added to data source.')
+  }
+})
+
+// 3. Get player by id: returns the player to data source
+api.get('/player/:id', (req, res) => {
+  const id = req.params.id
+
+  if (isFinite(id) && id > 0 ) {
+      const found = db.players.find((player) => (player.id == id))
+      if (found) { res.json(found) }
+      else {
+        res.status(404).send('Player not found.')
+      }
+  } else {
+    res.status(400).send('Parameter "id" is not a valid number.')
+  }
+  
+})
+
+// 4. Arm a player with an object in its bag
+api.patch('/player/:bag', (req, res) => {
+
+})
+
 
 module.exports = api;
